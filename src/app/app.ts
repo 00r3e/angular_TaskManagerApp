29fc5c745332f51e0c0751e0538d0706f0c67a90
ore s <000.r3e@gmail.com>
2026-02-20 12:11:03 +0200

    Add navbar and routing fixes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBar } from './nav-bar/nav-bar';
 import { TaskManager } from './task-manager/task-manager';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TaskManager],
+  imports: [RouterOutlet, NavBar, TaskManager],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
